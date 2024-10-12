d450d2cabe6d42caca734fe4a5f015d6626ed0a2
Marcos Daniel <marcosnumber11@gmail.com>
2024-10-12 19:51:02 -0300

    feat: Conexao Back_Front, criacao de usuario na pagina de cadastro

--- client/src/pages/Cadastro/index.tsx
+++ client/src/pages/Cadastro/index.tsx
@@ -6,17 +6,52 @@ import {
   TextField, 
   Typography 
 } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-import Login from '../Login';
 import { useState } from 'react';
 import './style.css'; 
+import { createUser } from '../../services/user/create';
 
 export default function Cadastro() {
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    try{
 
+      if(String(data.get('password')) !== String(data.get('passwordConfirm'))){
+        throw new Error('Senhas não coincidem')
+      }
+      const password = String(data.get('password')); 
+
+      if(password.length < 6)
+        throw new Error('Senha muito curta')
+
+      const hasLowercase = /[a-z]/.test(password);
+      const hasUppercase = /[A-Z]/.test(password);
+      const hasNumber = /\d/.test(password);
+  
+      if(!hasLowercase || !hasUppercase || !hasNumber)
+        throw new Error('Senha deve conter letras maiúsculas, minúsculas e números')
+
+      const body = { 
+          email : String(data.get('email')),
+          password : String(data.get('password')),
+          name : data.get('firstName') + ' ' + data.get('lastName')
+      };
+
+      await createUser(body);
+      alert('Usuário criado com sucesso');
+      navigate('/login');
+    }catch(err : any){
+      alert(err.message);
+    }
+
+  }
   return (
-    <Box className="container">
+    <Box component="form" className="container" onSubmit={handleSubmit}>
         <Box className="form-container">
           <Box className="form">
               <Box className="circle"></Box>
@@ -28,11 +63,11 @@ export default function Cadastro() {
                   <Box>
                     <Typography variant="inherit" className="subheader">Nome: </Typography>
                     <Box className="cluster">
-                      <TextField label="Primeiro nome" variant="outlined" fullWidth />
-                      <TextField label="Último nome" variant="outlined" fullWidth />
+                      <TextField label="Primeiro nome" variant="outlined" name="firstName" fullWidth />
+                      <TextField label="Último nome" variant="outlined" name="lastName" fullWidth />
                     </Box>
                   </Box>
-                  <TextField label="Email" variant="outlined" type="email" fullWidth />
+                  <TextField label="Email" variant="outlined" type="email" name="email" fullWidth />
                   
                   <Box>
                     <Typography variant="inherit" className="subheader">Senha: </Typography>
@@ -42,12 +77,14 @@ export default function Cadastro() {
                           variant="outlined" 
                           type={showPassword ? 'text' : 'password'} 
                           fullWidth 
+                          name = "password"
                       /> 
                       <TextField 
                           label="Confirme sua senha" 
                           variant="outlined" 
                           type={showPassword ? 'text' : 'password'} 
                           fullWidth 
+                          name= "passwordConfirm"
                       />
                     </Box>
                   </Box>
@@ -71,6 +108,7 @@ export default function Cadastro() {
                 <Button 
                     variant="contained" 
                     className="submit-btn"
+                    type='submit'
                     sx={
                       {
                         borderRadius: '50px',
