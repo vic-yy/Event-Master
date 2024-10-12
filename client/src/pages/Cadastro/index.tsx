225f9572da63f0a497aa4cb274d088ec78adbd3c
Marcos Daniel <marcosnumber11@gmail.com>
2024-10-12 08:25:16 -0300

    feat: Design basico da pagina de cadastro

--- client/src/pages/Cadastro/index.tsx
+++ client/src/pages/Cadastro/index.tsx
@@ -1,3 +1,98 @@
-export default function Cadastro(){
-    return <h1>You are at Sign Up Page!</h1>
+import { 
+  Box, 
+  Button, 
+  Checkbox, 
+  FormControlLabel, 
+  TextField, 
+  Typography 
+} from '@mui/material';
+import { useState } from 'react';
+import './style.css'; 
+
+export default function Cadastro() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <Box className="container">
+        <Box className="form-container">
+          <Box className="form">
+              <Box className="circle"></Box>
+              <Typography variant="h4" className="form-header">
+                  Cadastro
+              </Typography>
+
+              <Box className="form-fields">
+                  <Box>
+                    <Typography variant="inherit" className="subheader">Nome: </Typography>
+                    <Box className="cluster">
+                      <TextField label="Primeiro nome" variant="outlined" fullWidth />
+                      <TextField label="Último nome" variant="outlined" fullWidth />
+                    </Box>
+                  </Box>
+                  <TextField label="Email" variant="outlined" type="email" fullWidth />
+                  
+                  <Box>
+                    <Typography variant="inherit" className="subheader">Senha: </Typography>
+                    <Box className="cluster">
+                      <TextField 
+                          label="Senha" 
+                          variant="outlined" 
+                          type={showPassword ? 'text' : 'password'} 
+                          fullWidth 
+                      /> 
+                      <TextField 
+                          label="Confirme sua senha" 
+                          variant="outlined" 
+                          type={showPassword ? 'text' : 'password'} 
+                          fullWidth 
+                      />
+                    </Box>
+                  </Box>
+                  <Typography variant="body2" className="password-hint">
+                      Use 8 ou mais caracteres incluindo letras e números
+                  </Typography>
+
+                  <FormControlLabel id='show-password'
+                      control={
+                          <Checkbox 
+                              checked={showPassword} 
+                              onChange={() => setShowPassword(!showPassword)} 
+                              className="checkbox"
+                              sx={{ color: 'black', '&.Mui-checked': { color: 'black' } }} 
+                          />
+                      } 
+                      label="Mostrar senha" 
+                  />
+              </Box>
+              <Box className="submit-final">
+                <Button 
+                    variant="contained" 
+                    className="submit-btn"
+                    sx={
+                      {
+                        borderRadius: '50px',
+                        padding: '20px 50px',
+                        fontWeight: 'bold',
+                        fontSize: '1rem',
+                        backgroundColor: '#C4C4C4',
+                        '&:hover': {
+                          backgroundColor: '#515151',
+                        }
+                      }
+                    }
+                  >
+                    Criar Conta
+                </Button>
+
+                <Typography variant="caption" className="login-link">
+                    Já possui uma conta? Entre
+                </Typography>
+              </Box>
+        </Box>
+        <Box className="image-container">
+            <img className="logo" src='./src/assets/logo.jpeg'/>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
