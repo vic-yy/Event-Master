e8fa6cb293b91fb0a09f84cc97b0c6208c9fa14a
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-20 14:43:20 -0300

    Colocando os routers no index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,13 @@ import { app } from '../config/expressConfig';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import userRouter from './domains/User/controllers/userIndex';
+import eventRouter from './domains/Event/controllers/eventIndex';
+import participantRouter from './domains/Participant/controllers/participantIndex';
 
+app.use('/api/user', userRouter);
+app.use('/api/event', eventRouter);
+app.use('/api/participant', participantRouter);
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor hosteado na porta ' + process.env.PORT);
