a4aed6194f03477722204d484b9cea99b0a56046
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-20 15:05:44 -0300

    Upando migrações do branch de eventos

---
+++ api/prisma/migrations/20241009180354_01_add_event_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Event` (
+    `eventId` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `create_at` DATETIME(3) NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    PRIMARY KEY (`eventId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
