c9dbe6359a884767d3de057f6d345b8dea9d9601
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-20 15:12:10 -0300

    Removendo router do expressConfig.ts

--- api/config/expressConfig.ts
+++ api/config/expressConfig.ts
@@ -1,7 +1,6 @@
 import cors, { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
-import userRouter from '../src/domains/User/controllers/userIndex';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -18,7 +17,5 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(cookieParser());
-
-app.use('/api/user', userRouter);
 
 export default app;
