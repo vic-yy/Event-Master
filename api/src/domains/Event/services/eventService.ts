880bbce46e69bb2f38b1720e49579df6430f45fd
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-20 15:03:16 -0300

    Upando eventService.ts da branch de eventos

---
+++ api/src/domains/Event/services/eventService.ts
@@ -0,0 +1,48 @@
+import { Event } from '@prisma/client';
+import prisma from '../../../../database/prismaClient';
+import { QueryError } from '../../../../errors/QueryError';
+
+class EventService {
+    async createEvent(body: {title: string, description: string, location: string, date: Date}) {
+        const newEvent = await prisma.event.create({data: {...body, date: new Date(body.date)}});
+        return newEvent;
+    }
+    
+    async getEventById(eventId: number) {
+        const event = await prisma.event.findUnique({where: {eventId}});
+        if(!event){
+            throw new QueryError('eventNotFound');
+        }
+        return event;
+    }
+
+    async getEventByTitle(title: string) {
+        const event = await prisma.event.findFirst({where: {title}});
+        return event;
+    }
+    
+    async getAllEvents() {
+        const events = await prisma.event.findMany();
+        return events;
+    }
+    
+    async updateEvent(eventId: number, body: {name?: string, description?: string, date?: Date}) {
+        const event = await prisma.event.findUnique({where: {eventId}});
+        if(!event){
+            throw new QueryError('eventNotFound');
+        }
+        const updatedEvent = await prisma.event.update({where: {eventId}, data: body});
+        return updatedEvent;
+    }
+    
+    async deleteEvent(eventId: number) {
+        const event = await prisma.event.findUnique({where: {eventId}});
+        if(!event){
+            throw new QueryError('eventNotFound');
+        }
+        await prisma.event.delete({where: {eventId}});
+        return event;
+    }
+};
+
+export default new EventService();
