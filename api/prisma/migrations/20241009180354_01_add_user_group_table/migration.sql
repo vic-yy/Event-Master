7bf60874b54efa1618b67c47af7a17ba5aa9ee5c
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-28 10:28:17 -0300

    Criando migração da tabela de usuário-grupo

---
+++ api/prisma/migrations/20241009180354_01_add_user_group_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `User_Group` (
+    `user_groupId` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `groupId` INTEGER NOT NULL,
+    `role` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `user_group_unique` (`userId`, `groupId`),
+    PRIMARY KEY (`user_groupId`),
+
+    CONSTRAINT `fk_user` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE,
+    CONSTRAINT `fk_group` FOREIGN KEY (`groupId`) REFERENCES `Group` (`groupId`) ON DELETE CASCADE
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
