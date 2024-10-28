3bb1d58489832466f0a5c43859b1bd88ca29efa4
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-28 12:31:52 -0300

    Adicionando o atributo email na migração da tabela de participante

--- api/prisma/migrations/20241009180354_01_add_participant_table/migration.sql
+++ api/prisma/migrations/20241009180354_01_add_participant_table/migration.sql
@@ -3,11 +3,13 @@ CREATE TABLE `Participant` (
     `participantId` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
     `eventId` INTEGER NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
     `role` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `user_event_unique` (`userId`, `eventId`),
+    UNIQUE INDEX `user_event_unique` (`userId`, `eventId`, `email`),
     PRIMARY KEY (`participantId`),
 
+    CONSTRAINT `fk_email` FOREIGN KEY (`email`) REFERENCES `User` (`email`) ON DELETE CASCADE,
     CONSTRAINT `fk_user` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE,
     CONSTRAINT `fk_event` FOREIGN KEY (`eventId`) REFERENCES `Event` (`eventId`) ON DELETE CASCADE
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
