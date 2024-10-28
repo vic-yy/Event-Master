b168b3b2c6466d7c568a2e5aee20f935df797712
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-28 12:20:56 -0300

    Adicionando o atributo email na migração da tabela de usuário-grupo

--- api/prisma/migrations/20241009180354_01_add_user_group_table/migration.sql
+++ api/prisma/migrations/20241009180354_01_add_user_group_table/migration.sql
@@ -3,11 +3,13 @@ CREATE TABLE `User_Group` (
     `user_groupId` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
     `groupId` INTEGER NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
     `role` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `user_group_unique` (`userId`, `groupId`),
+    UNIQUE INDEX `user_group_unique` (`userId`, `groupId`, `email`),
     PRIMARY KEY (`user_groupId`),
 
+    CONSTRAINT `fk_email` FOREIGN KEY (`email`) REFERENCES `User` (`email`) ON DELETE CASCADE,
     CONSTRAINT `fk_user` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE,
     CONSTRAINT `fk_group` FOREIGN KEY (`groupId`) REFERENCES `Group` (`groupId`) ON DELETE CASCADE
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
