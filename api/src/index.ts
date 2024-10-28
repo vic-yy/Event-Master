e14dcdbac36b0d7a096683f6c95ca36b0ae15de3
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-28 14:36:38 -0300

    Atualizando index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,10 +5,16 @@ dotenv.config();
 import userRouter from './domains/User/controllers/userIndex';
 import eventRouter from './domains/Event/controllers/eventIndex';
 import participantRouter from './domains/Participant/controllers/participantIndex';
+import groupRouter from './domains/Participant/controllers/groupIndex';
+import userGroupRouter from './domains/Participant/controllers/userGroupIndex';
+import eventGroupRouter from './domains/Participant/controllers/eventGroupIndex';
 
 app.use('/api/user', userRouter);
 app.use('/api/event', eventRouter);
 app.use('/api/participant', participantRouter);
+app.use('/api/group', groupRouter);
+app.use('/api/userGroup', userGroupRouter);
+app.use('/api/eventGroup', eventGroupRouter);
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor hosteado na porta ' + process.env.PORT);
