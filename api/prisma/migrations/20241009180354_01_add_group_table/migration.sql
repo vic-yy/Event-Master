9a9ff6ec12d6de4b756a0b9dd1337633ef20242a
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-10-28 10:34:38 -0300

    Mudando a direção do projeto de grupo

--- api/prisma/migrations/20241009180354_01_add_group_table/migration.sql
+++ api/prisma/migrations/20241009180354_01_add_group_table/migration.sql
@@ -1,7 +1,6 @@
 -- CreateTable
 CREATE TABLE `Group` (
     `groupId` INTEGER NOT NULL AUTO_INCREMENT,
-    `eventId` INTEGER NOT NULL,
     `title` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     PRIMARY KEY (`groupId`)
