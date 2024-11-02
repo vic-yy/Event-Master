03a11a0dea82bb436705d004926096ea5876d975
vic-dti <victor.yano@dtidigital.com.br>
2024-11-02 11:53:04 -0300

    update: group

---
+++ api/src/domains/Group/services/groupService.ts
@@ -0,0 +1,48 @@
+import { Group } from '@prisma/client';
+import prisma from '../../../../database/prismaClient';
+import { QueryError } from '../../../../errors/QueryError';
+
+class GroupService {
+    async createGroup(body: {title: string, description: string}) {
+        const newGroup = await prisma.group.create({data: {...body}});
+        return newGroup;
+    }
+    
+    async getGroupById(groupId: number) {
+        const group = await prisma.group.findUnique({where: {groupId}});
+        if(!group){
+            throw new QueryError('groupNotFound');
+        }
+        return group;
+    }
+
+    async getGroupByTitle(title: string) {
+        const group = await prisma.group.findFirst({where: {title}});
+        return group;
+    }
+    
+    async getAllGroups() {
+        const groups = await prisma.group.findMany();
+        return groups;
+    }
+    
+    async updateGroup(groupId: number, body: {title?: string, description?: string}) {
+        const group = await prisma.group.findUnique({where: {groupId}});
+        if(!group){
+            throw new QueryError('groupNotFound');
+        }
+        const updatedGroup = await prisma.group.update({where: {groupId}, data: body});
+        return updatedGroup;
+    }
+    
+    async deleteGroup(groupId: number) {
+        const group = await prisma.group.findUnique({where: {groupId}});
+        if(!group){
+            throw new QueryError('groupNotFound');
+        }
+        await prisma.group.delete({where: {groupId}});
+        return group;
+    }
+};
+
+export default new GroupService();
--- api/src/index.ts
+++ api/src/index.ts
@@ -6,10 +6,12 @@ import userRouter from './domains/User/controllers/userIndex';
 import eventRouter from './domains/Event/controllers/eventIndex';
 import errorHandler from './middlewares/errorHandler';
 import participantRouter from './domains/Participant/controllers/participantIndex';
+import groupRouter from './domains/Group/controllers/groupIndex';
 
 app.use('/api/user', userRouter);
 app.use('/api/participant', participantRouter);
 app.use('/api/event', eventRouter);
+app.use('/api/group', groupRouter);
 
 app.use(errorHandler);
 
