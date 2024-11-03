b30b2c0ff92dd00dbb5504e88c8f73066b1c1ffb
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-11-03 16:00:05 -0300

    Corrigindo eventGroupService.ts

--- api/src/domains/Event_Group/services/eventGroupService.ts
+++ api/src/domains/Event_Group/services/eventGroupService.ts
@@ -9,7 +9,7 @@ import { TokenError } from '../../../../errors/TokenError';
 
 class EventGroupService {
   async protectEventGroup(body: Partial<Event_Group>) {
-    const event_group = await prisma.event_group.findUnique({where: {event_groupId: body.event_groupId}});
+    const event_group = await prisma.event_Group.findUnique({where: {event_groupId: body.event_groupId}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
@@ -22,16 +22,16 @@ class EventGroupService {
   }
 
   async createEventGroup(body: {eventId: number, groupId: number}) {
-    const sameEventGroup = await prisma.event_group.findUnique({where: {eventId_groupId: {eventId: body.eventId, groupId: body.groupId}}});
+    const sameEventGroup = await prisma.event_Group.findUnique({where: {eventId_groupId: {eventId: body.eventId, groupId: body.groupId}}});
     if(sameEventGroup){
         throw new QueryError('event_groupAlreadyExists');
     }
-    const newEventGroup = await prisma.event_group.create({data: {...body}});
+    const newEventGroup = await prisma.event_Group.create({data: {...body}});
     return newEventGroup;
   }
 
   async getEventGroupById(event_groupId: number) {
-    const event_group = await prisma.event_group.findUnique({where: {event_groupId}});
+    const event_group = await prisma.event_Group.findUnique({where: {event_groupId}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
@@ -39,7 +39,7 @@ class EventGroupService {
   }
 
   async getEventGroupByEventId(eventId: number) {
-    const event_group = await prisma.event_group.findMany({where: {eventId}});
+    const event_group = await prisma.event_Group.findMany({where: {eventId}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
@@ -47,7 +47,7 @@ class EventGroupService {
   }
 
   async getEventGroupByGroupId(groupId: number) {
-    const event_group = await prisma.event_group.findMany({where: {groupId}});
+    const event_group = await prisma.event_Group.findMany({where: {groupId}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
@@ -55,7 +55,7 @@ class EventGroupService {
   }
 
   async getEventGroupByEventIdGroupId(eventId: number, groupId: number) {
-    const event_group = await prisma.event_group.findUnique({where: {eventId_groupId: {eventId, groupId}}});
+    const event_group = await prisma.event_Group.findUnique({where: {eventId_groupId: {eventId, groupId}}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
@@ -63,58 +63,58 @@ class EventGroupService {
   }
 
   async getAllEventGroups() {
-    const event_groups = await prisma.event_group.findMany();
+    const event_groups = await prisma.event_Group.findMany();
     return event_groups;
   }
 
   async updateEventGroup(event_groupId: number, body: {eventId?: number, groupId?: number}) {
-    const event_group = await prisma.event_group.findUnique({where: {event_groupId}});
+    const event_group = await prisma.event_Group.findUnique({where: {event_groupId}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
     if (body.eventId && body.groupId) {
-      const sameEventGroup = await prisma.event_group.findUnique({where: {eventId_groupId: {eventId: body.eventId, groupId: body.groupId}}});
+      const sameEventGroup = await prisma.event_Group.findUnique({where: {eventId_groupId: {eventId: body.eventId, groupId: body.groupId}}});
       if (sameEventGroup && sameEventGroup.event_groupId !== event_groupId) {
         throw new QueryError('event_groupAlreadyExists');
       }
     }
-    const updatedEventGroup = await prisma.event_group.update({where: {event_groupId}, data: body});
+    const updatedEventGroup = await prisma.event_Group.update({where: {event_groupId}, data: body});
     return updatedEventGroup;
   }
 
   async deleteEventGroupById(event_groupId: number) {
-    const event_group = await prisma.event_group.findUnique({where: {event_groupId}});
+    const event_group = await prisma.event_Group.findUnique({where: {event_groupId}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
-    await prisma.event_group.delete({where: {event_groupId}});
+    await prisma.event_Group.delete({where: {event_groupId}});
     return event_group;
   }
 
   async deleteEventGroupByEventId(eventId: number) {
-    const event_group = await prisma.event_group.findMany({where: {eventId}});
+    const event_group = await prisma.event_Group.findMany({where: {eventId}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
-    await prisma.event_group.delete({where: {eventId}});
+    await prisma.event_Group.delete({where: {eventId}});
     return event_group;
   }
 
   async deleteEventGroupByGroupId(groupId: number) {
-    const event_group = await prisma.event_group.findMany({where: {groupId}});
+    const event_group = await prisma.event_Group.findMany({where: {groupId}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
-    await prisma.event_group.delete({where: {groupId}});
+    await prisma.event_Group.delete({where: {groupId}});
     return event_group;
   }
 
   async deleteEventGroupByEventIdGroupId(eventId: number, groupId: number) {
-    const event_group = await prisma.event_group.findUnique({where: {eventId_groupId: {eventId, groupId}}});
+    const event_group = await prisma.event_Group.findUnique({where: {eventId_groupId: {eventId, groupId}}});
     if(!event_group){
         throw new QueryError('event_groupNotFound');
     }
-    await prisma.event_group.delete({where: {eventId_groupId: {eventId, groupId}}});
+    await prisma.event_Group.delete({where: {eventId_groupId: {eventId, groupId}}});
     return event_group;
   }
 }
