69388a5554401b258a66602a6acd88a8f5077a00
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-11-03 16:47:27 -0300

    Atualizando InputValidator.ts

--- api/src/middlewares/InputValidator.ts
+++ api/src/middlewares/InputValidator.ts
@@ -23,3 +23,27 @@ export function invalidInputValidator(body: any) {
       throw new InvalidParamError('invalidFormat', 'password'); 
     }
 }
+
+export function isValidDate(dateTimeString: string ) {
+ 
+    const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]) ([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/;
+
+    if (!regex.test(dateTimeString)) {
+        throw new InvalidParamError('invalidFormat', 'date');
+    }
+	const [datePart, timePart] = dateTimeString.split(' ');
+    const [year, month, day] = datePart.split('-').map(Number);
+    const [hours, minutes, seconds] = timePart.split(':').map(Number);
+	
+	const date = new Date(year, month - 1, day, hours, minutes, seconds);
+
+    const ok = (date.getFullYear() === year &&
+           date.getMonth() === month - 1 &&
+           date.getDate() === day &&
+           date.getHours() === hours &&
+           date.getMinutes() === minutes &&
+           date.getSeconds() === seconds);
+
+	if (!ok) new InvalidParamError('invalidFormat', 'date');
+	return date;
+} 
