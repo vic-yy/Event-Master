3bae02876e11cb15321dfab183505867241061c9
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-11-06 20:58:26 -0300

    Update create.ts

--- client/src/services/userGroup/create.ts
+++ client/src/services/userGroup/create.ts
@@ -1,12 +1,12 @@
 import { api } from '../api';
 
 interface ICreateUserGroup{
-    userId: number
-    groupId: number
-    role: string
+ userId: number
+ groupId: number
+ role: string
 }
 
 
 export async function createUserGroup(body: ICreateUserGroup) {
-    return await api.post(/userGroup/create, body)
+ return await api.post(`/userGroup/create`, body)
 }
