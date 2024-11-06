443d290ceed914ccbab6c80aef5baa4cf5a9cae9
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-11-06 16:54:07 -0300

    Create create.ts

---
+++ client/src/services/userGroup/create.ts
@@ -0,0 +1,12 @@
+import { api } from '../api';
+
+interface ICreateUserGroup{
+    userId: number
+    groupId: number
+    role: string
+}
+
+
+export async function createUserGroup(body: ICreateUserGroup) {
+    return await api.post(/userGroup/create, body)
+}
