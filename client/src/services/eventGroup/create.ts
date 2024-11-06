c5aee2091fd4a1dfa23ee00226c21565c6c36d92
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-11-06 20:59:00 -0300

    Update create.ts

--- client/src/services/eventGroup/create.ts
+++ client/src/services/eventGroup/create.ts
@@ -1,11 +1,11 @@
 import { api } from '../api';
 
 interface ICreateEventGroup{
-    eventId: number
-    groupId: number
+ eventId: number
+ groupId: number
 }
 
 
 export async function createEventGroup(body: ICreateEventGroup) {
-    return await api.post(/eventGroup/create, body)
+ return await api.post(`/eventGroup/create`, body)
 }
