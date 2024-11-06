0c1afc61292dc2badf79072549393da984c1766f
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2024-11-06 20:57:34 -0300

    Update get.ts

--- client/src/services/group/get.ts
+++ client/src/services/group/get.ts
@@ -1,9 +1,5 @@
 import { api } from '../api';
 
-interface IGetGroup{
-    title: string;
-}
-
-export async function getGroupByTitle(body: IGetGroup) {
-    return await api.get(/group/getGroupByTitle, body)
+export async function getGroupByTitle(title: string) {
+ return await api.get(`/group/getByTitle/${title}`)
 }
